5703d7c548c5f824d2eca8f262cd2ff48af8d43d
Ben Brenkman <JustBrenkman@gmail.com>
2020-02-22 09:31:10 -0700

    timestamp

--- Examples/AGV/agv_orb.cc
+++ Examples/AGV/agv_orb.cc
@@ -90,7 +90,7 @@ int main(int argc, char **argv)
 
         cout << timestamp << endl;
         // Pass the image to the SLAM system
-        SLAM.TrackRGBD(imRGB,imD,timestamp);
+        SLAM.TrackRGBD(imRGB,imD,timestamp * 1000);
 
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
