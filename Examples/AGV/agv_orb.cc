86afd8b97afdb68fc2b69f028908ee34c9cd7905
Ben Brenkman <JustBrenkman@gmail.com>
2020-02-22 08:38:30 -0700

    timestamp

--- Examples/AGV/agv_orb.cc
+++ Examples/AGV/agv_orb.cc
@@ -79,9 +79,11 @@ int main(int argc, char **argv)
         std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
 #endif
         double timestamp = frames.get_timestamp();
-        cout << timestamp << endl;
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t1 - 0).count();
+
+        cout << ttrack << endl;
         // Pass the image to the SLAM system
-        SLAM.TrackRGBD(imRGB,imD,timestamp);
+        SLAM.TrackRGBD(imRGB,imD,ttrack);
 
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
@@ -100,31 +102,4 @@ int main(int argc, char **argv)
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");   
 
     return 0;
-}
-
-void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageFilenamesRGB,
-                vector<string> &vstrImageFilenamesD, vector<double> &vTimestamps)
-{
-    ifstream fAssociation;
-    fAssociation.open(strAssociationFilename.c_str());
-    while(!fAssociation.eof())
-    {
-        string s;
-        getline(fAssociation,s);
-        if(!s.empty())
-        {
-            stringstream ss;
-            ss << s;
-            double t;
-            string sRGB, sD;
-            ss >> t;
-            vTimestamps.push_back(t);
-            ss >> sRGB;
-            vstrImageFilenamesRGB.push_back(sRGB);
-            ss >> t;
-            ss >> sD;
-            vstrImageFilenamesD.push_back(sD);
-
-        }
-    }
 }
